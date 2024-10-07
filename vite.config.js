cb307b82b642b10d6f097d1ea659b10c3076970b
karthikeyan manimaran <karthikeyan14june@gmail.com>
2024-10-07 14:32:55 +0530

    tests

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allow access from external devices
+    port: 5173,      // Your desired port
+    strictPort: true, // Prevents automatic port switching if the port is already in use
+},
 })
