3e64800e643bb65da145def0bfbc147fe6890ab3
karthik <karthikeyan14june@gmail.com>
2024-10-12 17:56:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react()
-  ],
-  server: {
-    host: '0.0.0.0', // Allow access from external devices
-    port: 5173,      // Your desired port
-    strictPort: true, // Prevents automatic port switching if the port is already in use
-  },
-});
+  plugins: [react()],
+  base: "/madras-mapper/"
+})
