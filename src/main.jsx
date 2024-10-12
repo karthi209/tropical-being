fecb5135597ad0f3beaed3001acc2c1d40730ad1
karthikeyan manimaran <karthikeyan14june@gmail.com>
2024-10-12 19:35:35 +0530

    tests

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 
@@ -9,8 +9,8 @@ const basename = import.meta.env.VITE_BASE_PATH;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter basename={basename}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
